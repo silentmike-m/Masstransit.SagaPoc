3e6bf993bad25033dc0c40c9f48394806c8bdfc3
Michal Cichowski <michal.cichowski@bytespack.com>
2023-05-22 12:03:47 +0200

    fix publisher context

--- Masstransit.SagaPoc.Publisher/src/Masstransit.SagaPoc.Publisher.Infrastructure/MemoryDb/Services/CustomersDb.cs
+++ Masstransit.SagaPoc.Publisher/src/Masstransit.SagaPoc.Publisher.Infrastructure/MemoryDb/Services/CustomersDb.cs
@@ -1,9 +1,10 @@
 ﻿namespace Masstransit.SagaPoc.Publisher.Infrastructure.MemoryDb.Services;
 
 using Masstransit.SagaPoc.Publisher.Domain.Entities;
+using Masstransit.SagaPoc.Publisher.Infrastructure.MemoryDb.Interfaces;
 using CustomerId = Guid;
 
-internal sealed class Context
+internal sealed class Context : IContext
 {
     public Dictionary<CustomerId, CustomerEntity> Customers { get; private set; }
 
